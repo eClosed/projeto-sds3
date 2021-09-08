7f6c0f19355a364e806877b40affa9acbc90a70d
eClosed <evtaalk@gmail.com>
2021-09-08 02:48:27 -0300

    NavBar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,14 @@
+import NavBar from "components/NavBar";
+
 function App() {
   return (
-    <div>
-      <h1 className="text-primary">Have a nice day!</h1>
-    </div>
-  );
+    <>
+      <NavBar />
+      <div className="container">
+        <h1 className="text-primary">Have a nice day!</h1>
+      </div>
+    </>
+        );
 }
 
-export default App;
+        export default App;
