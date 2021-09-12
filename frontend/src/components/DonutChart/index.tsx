010729994209fd6fb8fb9c8a87eac2a1d4f8c459
eClosed <evtaalk@gmail.com>
2021-09-12 14:29:40 -0300

    BarChart integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -16,15 +16,15 @@ const DonutChart = () => {
 
     useEffect(() => {
         axios.get(`${BASE_URL}/sales/amount-by-seller`)
-        .then(response => {
-            const data = response.data as SaleSum[];
-            const myLabels = data.map(x => x.sellerName);
-            const mySeries = data.map(x => x.sum);
+            .then(response => {
+                const data = response.data as SaleSum[];
+                const myLabels = data.map(x => x.sellerName);
+                const mySeries = data.map(x => x.sum);
 
-            setChartData({ labels: myLabels, series: mySeries });
-        });
-    } , []);
-    
+                setChartData({ labels: myLabels, series: mySeries });
+            });
+    }, []);
+
     // FORMA ERRADA
     // let chartData: ChartData = { labels: [], series: [] }
 
@@ -38,12 +38,6 @@ const DonutChart = () => {
     //         chartData = { labels: myLabels, series: mySeries }
     //         console.log(chartData);
     //     });
-
-
-    //const mockData = {
-    //    series: [477138, 499928, 444867, 220426, 473088],
-    //    labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-    //}
 
     const options = {
         legend: {
